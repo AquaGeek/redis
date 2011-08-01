e0551593a3583edac534521b4432bc6aefd57b4f
Tyler Stromberg <tylers@mac.com>
2011-08-01 15:58:30 -0300

    Minor language cleanup

--- src/anet.c
+++ src/anet.c
@@ -132,7 +132,7 @@ static int anetCreateSocket(char *err, int domain) {
     }
 
     /* Make sure connection-intensive things like the redis benckmark
-     * will be able to close/open sockets a zillion of times */
+     * will be able to close/open sockets a zillion times */
     if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) == -1) {
         anetSetError(err, "setsockopt SO_REUSEADDR: %s", strerror(errno));
         return ANET_ERR;
